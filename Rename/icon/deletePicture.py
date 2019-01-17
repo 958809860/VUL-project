975758ff45f8f98cda57b9fadffbf70bad9e6d52
yangxuan.xa <958809860@qq.com>
2019-01-17 18:43:58 +0800

    '增加提示'

--- Rename/icon/deletePicture.py
+++ Rename/icon/deletePicture.py
@@ -2,16 +2,17 @@
 #-*- coding: utf-8 -*-
 
 import os,sys,shutil
-movepath = r'E:\gitclone\VUL-project\Rename\icon\新建文件夹'
+movepath = r'E:\gitclone\VUL-project\Rename\icon\新建文件夹'    #将未用到的资源移动到此路径下
 
-#根据文件扩展名判断文件类型
-def endWith(s,*endstring):
+
+def endWith(s,*endstring):          #根据文件扩展名判断文件类型
     array = map(s.endswith,endstring)
     if True in array:
         return True
     else:
         return False
-def get_process_files(root_dir):
+
+def get_process_files(root_dir):    #获取文件路径
 
     file_list = []
     process_list=[]
@@ -38,20 +39,18 @@ def get_process_files(root_dir):
                     process_list.append(x)
     return process_list,picture_list
 
-def findstring (filename,string):
+def findstring (filename,string):       #在filename的文件中查全称string
 
     document = open(filename, 'r+', encoding = 'utf-8')
     for each_line in document:
         if each_line.find(string) != -1:   
-            return True
+            return True                 #string存在返回true
     document.close()
 
-
-
-def search_string(filename,string):
+def search_string(filename,string):     #在filename的文件中遍历string截掉‘_’后字符串
     if string.find('_') != -1:
         lenstring = len(string.split('_'))
-        movefilename = "_".join(string.split('_')[0:(lenstring-1)])  #将最后的_[]截掉
+        movefilename = "_".join(string.split('_')[0:(lenstring-1)])  #将最后的_字符串截掉
         #print('movefilename',movefilename)
         if findstring(filename,movefilename) == True:
             return True
@@ -59,17 +58,15 @@ def search_string(filename,string):
             if search_string(filename,movefilename) == True:
                 return True
 
-
-
 def count_files(root_dir,root_dir1,root_dir2):
         temp=get_process_files(root_dir)
         temp1=get_process_files(root_dir1)
         temp2=get_process_files(root_dir2)
         process_list=temp[0] + temp1[0] + temp2[0]  #所有json，ts文件的路径
-        picture_list=temp2[1]#所有png，jpg文件的路径
+        picture_list=temp2[1]                       #所有png，jpg文件的路径
         #print('\n\nprocess_list=',process_list,'\n\npicture_list=',picture_list)
         for num in range(len(picture_list)):
-            string = os.path.basename(picture_list[num]).split('.',1)[0] #获得一个png或jpg文件名
+            string = os.path.basename(picture_list[num]).split('.',1)[0] #获得png或jpg文件名
             account = 0
             for files in process_list:
                 #print(files,string)
@@ -87,9 +84,8 @@ def count_files(root_dir,root_dir1,root_dir2):
                             print('removed:',string,picture_list[num])
                 account += 1
 
- 
 if __name__=='__main__':
-    root_dir=r'E:\gitclone\VUL-project\Rename\icon\MahjongLobby_XDZPK\Egret\src'#TS文件路径
+    root_dir=r'E:\gitclone\VUL-project\Rename\icon\MahjongLobby_XDZPK\Egret\src'#TS文件路径    #sys.argv[1] 使用时加python
     root_dir1=r'E:\gitclone\VUL-project\Rename\icon\MahjongLobby_XDZPK\Egret\resource\lobbySkins'#EXML文件路径
     root_dir2=r'E:\gitclone\VUL-project\Rename\icon\MahjongLobby_XDZPK\resource'#图片路径
     count_files(root_dir,root_dir1,root_dir2)
