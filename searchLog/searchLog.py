60cc3b32fb40255c1a61935fb3d17e07afa0c5b8
yangxuan.xa <958809860@qq.com>
2019-01-21 15:03:46 +0800

    '脚本判断'

--- searchLog/searchLog.py
+++ searchLog/searchLog.py
@@ -7,16 +7,14 @@ TXT = r'./tdkserver.log.181219-15'
 
 document = open(TXT, 'r+', encoding = 'utf-8')
 count = 0
-
 def searchstring(each_line):
         for i in range(len(stringlist)):
                 if each_line.find(stringlist[i]) != -1:
                         pass
                 else:
                         return False
-
-for each_line in document:
-    count += 1 
-    if searchstring(each_line) != False:
-            print(count , each_line)
-
+if __name__=='__main__':
+        for each_line in document:
+                count += 1 
+                if searchstring(each_line) != False:
+                        print(count , each_line)
