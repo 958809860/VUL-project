5dad46854c598a771def9cc4e2e8dad16e492693
yangxuan.xa <958809860@qq.com>
2019-01-21 15:00:10 +0800

    '多字符串查找'

---
+++ searchLog/searchLog.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+
+stringlist = ['通辽','Cmd.ShowActionRoomCmd_CS','1103127']
+TXT = r'./tdkserver.log.181219-15'
+
+document = open(TXT, 'r+', encoding = 'utf-8')
+count = 0
+
+def searchstring(each_line):
+        for i in range(len(stringlist)):
+                if each_line.find(stringlist[i]) != -1:
+                        pass
+                else:
+                        return False
+
+for each_line in document:
+    count += 1 
+    if searchstring(each_line) != False:
+            print(count , each_line)
+
