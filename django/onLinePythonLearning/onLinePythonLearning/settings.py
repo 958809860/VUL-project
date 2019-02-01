025793bf603b7a185b42d930152df983c2c2783f
yangxuan.xa <958809860@qq.com>
2019-02-01 10:48:16 +0800

    Python+django用户登录与登录验证

--- django/onLinePythonLearning/onLinePythonLearning/settings.py
+++ django/onLinePythonLearning/onLinePythonLearning/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'onLinePythonLearning.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- django/onLinePythonLearning/questions/views.py
+++ django/onLinePythonLearning/questions/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Questions
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url = 'login/')
 def index(request):
     htmlCode = '<table border="1px">'
     for row in Questions.objects.all():
         htmlCode += ('<tr><td>' + str(row.id) + '<td><td>' + row.questionConttent + '<td><td>' + row.answer + '<td>') 
     htmlCode += '</table>'
     return HttpResponse(htmlCode) 
+
+def login(request):
+
+    try:
+        userName = request.POST['usr']
+        userPwd = request.POST['pwd']
+        user = auth.authenticate(username = userName,password = userPwd)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/check/')
+        else:
+            return render(request,'login.html',{'msg':'用户名或密码不正确'})
+    except:
+        return render(request,'login.html',{'msg':None})
