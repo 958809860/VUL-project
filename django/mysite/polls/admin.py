baeb0e70999e5bfb7c75ecbffca59f49deac22c0
yangxuan.xa <958809860@qq.com>
2019-03-10 20:43:48 +0800

    标题修改

--- django/mysite/polls/admin.py
+++ django/mysite/polls/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from polls.models import message
 from polls.models import addmessage
 # Register your models here.
+admin.site.site_title="晨会日志管理"
+admin.site.site_header="日志管理"
+admin.site.index_title="日志管理"
+
 class messageList(admin.ModelAdmin):
     list_display = ('id','username', 'password') # list
 admin.site.register(message, messageList)
