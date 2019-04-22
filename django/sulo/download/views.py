be57ec0c7f9a420cead001947c6f1398b6536ab7
yangxuan.xa <958809860@qq.com>
2019-04-23 00:00:25 +0800

    update

--- django/sulo/download/views.py
+++ django/sulo/download/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render,redirect
 from django.shortcuts import HttpResponse
 from .import models
 from functools import wraps
+from itertools import chain
 # Create your views here.
 def index(request):
-    productList = models.product.objects.all().values('id','name','desc')
-    # productLink = models.downloadlink.objects.get(id=productList.id)
-    print (type(productList))
-    # productList['link'] = productLink
-    result = productList
-    print (result)
-    return render(request, 'file.html',{'result': result})
+    result1 = []
+    result2 = []
+    all = len(models.product.objects.all())
+    for i in range(all):
+        productList = models.product.objects.filter(id = i+1).values('id','name','desc')
+        productLink = models.downloadlink.objects.filter(product_id= productList[0]['id']).values('id','product_id','product_link')
+
+        result1.append(productList)
+        result2.append(productLink)
+    print (result1,result2)
+    return render(request, 'file.html',{'result1': result1,'result2': result2})
